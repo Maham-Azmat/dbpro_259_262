b6028aba1ed61b85847eb26435ac5daaab684464
Maham-Azmat <mahabutt08@gmail.com>
2019-04-25 02:43:17 +0500

    login register

---
+++ loanmanagement/loanmanagementsystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(loanmanagementsystem.Startup))]
+namespace loanmanagementsystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
