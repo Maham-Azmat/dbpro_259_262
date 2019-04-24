dd32fe7ee445e85e2203da1ed28e9e2952776f28
Maham-Azmat <mahabutt08@gmail.com>
2019-04-24 11:56:24 +0500

    login register of admin and customer

---
+++ loanmanagementsystem/loanmanagementsystem/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace loanmanagementsystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //Database_Connection.get_instance().connectionstring = "Data Source=HAIER-PC;Initial Catalog=DB64;Integrated Security=True";
+            //var con = Database_Connection.get_instance().Getconnection();
+            //con.Open();
+
+            //catch (Exception ex)
+            //{
+            //    throw(ex);
+            //}
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
