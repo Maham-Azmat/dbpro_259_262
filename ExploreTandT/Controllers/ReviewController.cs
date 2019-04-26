cf85d33594af11dfeff4533bf7cb6619742f66b7
KhadijaZahoor <khadijazahoor77@gmail.com>
2019-04-26 05:27:40 +0500

    Loan and Employee View

---
+++ ExploreTandT/Controllers/ReviewController.cs
@@ -0,0 +1,40 @@
+﻿using ExploreTandT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExploreTandT.Controllers
+{
+    public class ReviewController : Controller
+    {
+        // GET: Review
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Show the Reviews to all users 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ShowReview()
+        {
+
+            ExploreEntities d = new ExploreEntities();
+            var storeReviews = d.Reviews.ToList();
+            ShowReviewModel userl = new ShowReviewModel();
+
+            foreach (var t in storeReviews)
+            {
+                ReviewsModel r = new ReviewsModel();
+                r.Name = t.Name;
+                r.Reviews = t.Reviews;
+                userl.StoreReview.Add(r);
+
+            }
+            return View(userl);
+        }
+    }
+}
---
+++ ExploreTandT/Models/AllPackageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExploreTandT.Models
+{
+    public class AllPackageViewModel
+    {
+        [Required]
+        [Display(Name = "Package Name")]
+        [RegularExpression(@"^[a-zA-Z ]*$", ErrorMessage = "Name should be in Alphabets")]
+        public string Name { get; set; }
+
+
+        public string Category { get; set; }
+
+        [Required]
+        [Display(Name = "Places")]
+        [RegularExpression(@"^[a-zA-Z ]*$", ErrorMessage = "Place should be in Valid")]
+        public string Places {get; set;}
+
+        [Required(ErrorMessage = "Pleasee enter Valid Expense")]
+        [DataType(DataType.Currency)]
+        [Range(1, 9999, ErrorMessage = "Enter Range between 1 and 999")]
+        [RegularExpression(@"^(((\d{1})*))$", ErrorMessage = "Enter Valid Expense")]
+        public int Range { get; set; }
+
+        public string TourGuide { get; set; }
+        
+        public string Schedule { get; set; }
+
+        [Required]
+        [Display(Name = "Vehicle")]
+        
+        public string Vehicle { get; set; }
+
+        [Required]
+        [Display(Name = "Hotel")]
+        [RegularExpression(@"^[a-zA-Z ]*$", ErrorMessage = "Error")]
+        public string Hotel { get; set; }
+
+        public string Refreshments { get; set; }
+
+        public int packageId { get; set;}
+    }
+}
