7efc702838cc0b64a7d74205242fd26eafe01393
KhadijaZahoor <khadijazahoor77@gmail.com>
2019-04-28 03:54:17 +0500

    Policy and installment Accept/Reject Request and accept Terms & conditions select installment plan

---
+++ EmployeeLoan/ExploreTandT/Controllers/EmployeeLoanController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using System.Data.SqlClient;
+using ExploreTandT.Models;
+
+namespace ExploreTandT.Controllers
+{
+    public class EmployeeLoanController : Controller
+    {
+        // GET: EmployeeLoan
+        DB64Entities db = new DB64Entities();
+
+        public ActionResult Index(int id)
+        {
+            LoanViewModel req = new LoanViewModel();
+
+            foreach (Loan e in db.Loans)
+            {
+                if (e.Id == id)
+                {
+                    req.listofloans.Add(e);
+                    req.TermsAndConditions = e.TermsAndConditions;
+                }
+            }
+
+
+            return View(req);
+        }
+
+        public ActionResult Installment(int id)
+        {
+            AdminViewModel user = new AdminViewModel();
+            foreach (Installement u in db.Installements)
+            {
+                if (u.LoanId == id)
+                {
+                    user.listOfInstallements.Add(u);
+                }
+            }
+            return View(user);
+        }
+
+        public ActionResult AddDocuments(int id)
+        {
+            return View();
+        }
+
+        // GET: EmployeeLoan/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EmployeeLoan/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeLoan/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeLoan/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeLoan/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeLoan/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeLoan/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EmployeeLoan/ExploreTandT/Database_Connection.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace ExploreTandT
+{
+    public class Database_Connection
+    {
+        private static Database_Connection instance;
+        public string connectionstring;
+        private SqlConnection connection;
+
+        Database_Connection()
+        {
+
+        }
+
+        public static Database_Connection get_instance()
+        {
+            if (instance == null)
+            {
+                instance = new Database_Connection();
+            }
+            return instance;
+        }
+
+
+        public SqlConnection Getconnection()
+        {
+            connection = new SqlConnection(connectionstring);
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+            }
+            return connection;
+        }
+
+        public void close()
+        {
+            if (connection != null)
+            {
+                connection.Close();
+            }
+        }
+
+        public SqlDataReader Getdata(string query)
+        {
+            connection = Getconnection();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataReader data = cmd.ExecuteReader();
+            return data;
+
+        }
+
+        public int Executequery(string query)
+        {
+            connection = Getconnection();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            int row = cmd.ExecuteNonQuery();
+            return row;
+        }
+    }
+}
