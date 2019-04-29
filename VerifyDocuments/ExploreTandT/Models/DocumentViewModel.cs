17c733888167d765dced3c6e52667cf929c36217
Maham-Azmat <mahabutt08@gmail.com>
2019-04-29 15:02:35 +0500

    verify documents by admin, removing duplicate document submission of a particular loan

---
+++ VerifyDocuments/ExploreTandT/Models/DocumentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExploreTandT.Models
+{
+    public class DocumentViewModel
+    {
+        public HttpPostedFileBase ImageFile { get; set; }
+        public int Id;
+        public int Loanid;
+        public int Employeeid;
+        public string Document;
+        public int Status;
+        public int temp;
+        public string Title;
+    }
+}
