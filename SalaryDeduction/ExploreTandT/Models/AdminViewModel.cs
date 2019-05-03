b27ffbf8def2ad9f5e0227456cc0a8122b07d516
Maham-Azmat <mahabutt08@gmail.com>
2019-05-03 11:52:08 +0500

    salary deduction and admin account modified

---
+++ SalaryDeduction/ExploreTandT/Models/AdminViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExploreTandT.Models
+{
+    public class AdminViewModel
+    {
+        public List<Loan> listofloans = new List<Loan>();
+        public List<Employee> listofemployees = new List<Employee>();
+        public List<EmployeeLoan> listofEmployeeLoan = new List<EmployeeLoan>();
+        public List<Request> requests = new List<Request>();
+        public List<PolicyAndRule> listOfPolicies = new List<PolicyAndRule>();
+        public List<Installement> listOfInstallements = new List<Installement>();
+        public List<RepaymentSchedule> listOfRepaymentSchedule = new List<RepaymentSchedule>();
+        public List<Document> listOfDocumentstobeverified = new List<Document>();
+        public List<SalaryDeduction> Salarydeductlist = new List<SalaryDeduction>();
+
+
+
+    }
+}
